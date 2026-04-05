ee60d28005cf0134b6cd6689bc9175743469e609
Real <162379302+Realvoa@users.noreply.github.com>
2026-04-05 20:52:21 +0300

    Add MTProto Proxy implementation in proxy.py

---
+++ proxy.py
@@ -0,0 +1,27 @@
+import os
+import random
+import subprocess
+
+def gen_secret():
+    chars = "0123456789abcdef"
+    return "dd" + "".join(random.choice(chars) for _ in range(30))
+
+PORT = os.environ.get("PORT", "8080")
+SECRET = gen_secret()
+
+print("================================")
+print("MTProto Proxy Started 🗿")
+print("PORT:", PORT)
+print("SECRET:", SECRET)
+print("================================")
+
+subprocess.run([
+"/app/MTProxy/objs/bin/mtproto-proxy",
+"-u","nobody",
+"-p","8888",
+"-H",PORT,
+"-S",SECRET,
+"--aes-pwd","/app/MTProxy/proxy-secret",
+"/app/MTProxy/proxy-multi.conf",
+"-M","1"
+])
