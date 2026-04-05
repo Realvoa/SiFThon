aa9f70be34aa3e7f2f97ce259f5702d3b7c2d171
Real <162379302+Realvoa@users.noreply.github.com>
2026-04-05 21:13:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,35 +8,29 @@ PORT = int(os.environ.get("PORT", 8000))
 PROXY_PORT = 9000
 
 def gen_secret():
-    chars = "0123456789abcdef"
-    return "dd" + "".join(random.choice(chars) for _ in range(30))
+    chars="0123456789abcdef"
+    return "".join(random.choice(chars) for _ in range(32))
 
 SECRET = gen_secret()
 
-print("MTProto Proxy Started 🗿")
+print("Proxy started 🗿")
 print("PORT:", PROXY_PORT)
 print("SECRET:", SECRET)
 
-os.system("curl -s https://core.telegram.org/getProxySecret -o /app/MTProxy/proxy-secret")
-os.system("curl -s https://core.telegram.org/getProxyConfig -o /app/MTProxy/proxy-multi.conf")
-
 def start_proxy():
     subprocess.run([
-        "/app/MTProxy/objs/bin/mtproto-proxy",
-        "-u","nobody",
-        "-p","8888",
-        "-H",str(PROXY_PORT),
-        "-S",SECRET,
-        "-M","1",
-        "--aes-pwd","/app/MTProxy/proxy-secret",
-        "/app/MTProxy/proxy-multi.conf"
+        "./mtg",
+        "run",
+        "--bind",
+        f"0.0.0.0:{PROXY_PORT}",
+        SECRET
     ])
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Proxy Running")
+        self.wfile.write(b"OK")
 
 def start_http():
     server = HTTPServer(("0.0.0.0", PORT), Handler)
